66012fb9d9629999f6b8607c259c2e0ad6ffd307
Jun Nakajima <ikuta1919@gmail.com>
2020-08-20 23:59:42 +0900

    application_service、途中まで

--- functions/ShopBill/source/application/csv_to_space_separate_application.py
+++ functions/ShopBill/source/application/csv_to_space_separate_application.py
@@ -1,6 +1,9 @@
 import dataclasses
 
+from source.domain.models.csv_models.csv_cash_transaction_header import CsvCashTransactionHeader
+from source.domain.rules.transfer_rules import TransferRules
 from source.util.util import get_logger
+from tests.In_memory_code_repository import InMemoryCodeRepository
 
 logger = get_logger('INFO')
 
@@ -12,8 +15,16 @@ class CsvToSpaceSeparateApplication(object):
     bucket: str
     key: str
 
-    def csv_to_space_separate(self) -> None:
+    def csv_to_space_separate(self, file_path) -> None:
         """
         CSV→スペース区切り変換
         """
-        pass
+
+        # CSVモデルに変換
+        csv_models = CsvCashTransactionHeader.from_csv(file_path)
+
+        # ドメインモデルに変換
+        shop_monthly_sales = TransferRules(InMemoryCodeRepository()).to_shop_sales(csv_models)
+
+        # スペース区切りに変換する
+
